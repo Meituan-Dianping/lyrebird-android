e7c28c5d6185e5431928fce29ce10dd3c547c00c
yumiguan <yumiguan1009@gmail.com>
2018-08-21 14:49:08 +0800

    remove unecessary code in dubug

--- debug.py
+++ debug.py
@@ -1,14 +1,7 @@
 import lyrebird
 import pip
-import time
-import asyncio
-from asyncio import Event
-import threading
 
 
 if __name__ == '__main__':
     pip.main(['install', '.',  '--upgrade'])
     lyrebird.debug()
-
-    loop = asyncio.get_event_loop()
-    loop.run_forever()
