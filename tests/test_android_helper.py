e11aba98828d59570a8aaf48b359e1c57a5e12b3
yumiguan <41277086+yumiguan@users.noreply.github.com>
2018-08-31 11:28:44 +0800

    Feature/add new tag (#8)
    * add tags in readme
    
    * change version name
    
    * add empty line

--- tests/test_android_helper.py
+++ tests/test_android_helper.py
@@ -2,7 +2,7 @@ from lyrebird_android.android_helper import Device
 import pytest
 
 
-def test_package_name(self):
+def test_package_name():
     device_info_str = "00c34258893533x       device usb:337641472X product:bullhead model:Nexus_5X device:bullhead transport_id:24"
     device_info_dict = Device.from_adb_line(device_info_str)
     assert len(device_info_dict.model) > 0 
