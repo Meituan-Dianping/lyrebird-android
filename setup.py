8f6905181b2b1380ff05bf779a879074941a2b90
yumiguan <41277086+yumiguan@users.noreply.github.com>
2018-12-03 11:02:22 +0800

    solve get device ip error (#21)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='lyrebird-android',
-    version='0.2.3',
+    version='0.2.4',
     packages=['lyrebird_android'],
     url='https://github.com/meituan/lyrebird-android',
     author='HBQA',
