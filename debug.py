be05769c2eb94b1da7dca4b0b65af4980e007196
zhaoye <zhaoye@users.noreply.github.com>
2019-04-24 19:03:57 +0800

    Fix: device service crash (#25)
    * Fix: device service crash
    
    * fix codacy issue
    
    * fix codacy issue

--- debug.py
+++ debug.py
@@ -3,6 +3,11 @@ import pip
 
 
 if __name__ == '__main__':
-    pip.main(['install', '.',  '--upgrade'])
-    lyrebird.debug()
+    version_num = pip.__version__[:pip.__version__.find('.')]
+    if int(version_num) >= 10:
+        from pip import __main__
+        __main__._main(['install', '.',  '--upgrade'])
+    else:
+        pip.main(['install', '.',  '--upgrade'])
+    lyrebird.main()
 
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='lyrebird-android',
-    version='0.2.6',
+    version='0.2.7',
     packages=['lyrebird_android'],
     url='https://github.com/meituan/lyrebird-android',
     author='HBQA',
