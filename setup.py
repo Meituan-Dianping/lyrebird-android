157ab3fe8572f7900d5f65a9a9381baac3a08633
yumiguan <41277086+yumiguan@users.noreply.github.com>
2019-10-29 14:15:33 +0800

    add screenshot in device list (#32)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='lyrebird-android',
-    version='0.4.1',
+    version='0.4.2',
     packages=['lyrebird_android'],
     url='https://github.com/meituan/lyrebird-android',
     author='HBQA',
@@ -30,5 +30,4 @@ setup(
     install_requires=[
         'lyrebird'
     ]
-
 )
