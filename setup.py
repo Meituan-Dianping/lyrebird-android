7bbbbe52e4dff2c8d20c1dce52f23e7b6fbe56af
yumiguan <yumiguan1009@gmail.com>
2022-12-02 15:15:24 +0800

    Upgrade frontend dependencies socket.io-client and axios

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='lyrebird-android',
-    version='0.5.13',
+    version='0.5.14',
     packages=['lyrebird_android'],
     url='https://github.com/meituan/lyrebird-android',
     author='HBQA',
@@ -28,7 +28,7 @@ setup(
         ]
     },
     install_requires=[
-        'lyrebird'
+        'lyrebird>=2.10.3'
     ],
     extras_require={
         'dev': [
